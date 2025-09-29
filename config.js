2791912661701e89d7c45b62800a2bbc95ed8158
msu4ainiki <msu4ainiki@gmail.com>
2025-09-29 18:49:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const imageConfig = {
     leftTeamImages: [
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/Screenshot_20250928-234623~3.png', // PNG с прозрачностью
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759151720282.png',
-    'https://s297klg.storage.yandex.net/rdisk/4c4edb65037a4bc15808a878ee915984f32e664498257496a7cb498d5965bd5e/68da3268/ZQR_a0yN6_t_b6ztngYWwuEL_-X0xkhTt616cGVhMRpAm-HBRUAF2RxqnPJ7FMCwJdRibYJwGY4oOGx40pgARA==?uid=0&filename=Screenshot_20250928-234623~3.png&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&fsize=56804&hid=d851f711f3f4c28424aee760dc52daca&media_type=image&tknv=v3&etag=88ccee64f3f65ae8d9283533760ca7a9&ts=63feb6ba36a00&s=bf71474ec9ffcabab0b88b15903d7e6912b12d87008717d2c833bb87d4d9acb3&pb=U2FsdGVkX1-aAYEQhuGCaq910S-sA34N5N4zegsI0aLto7GLpiIILLIh97vf1zc6ucfeblD_210nL7ehGTWzHHUHnR4YA0tMAf87fMJzo6E'
+    'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759160744779.png'
 ],
     
     // Изображения для правой команды (Лыжники)
