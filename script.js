08991ef900962f0077e92725468cd259a803fea9
msu4ainiki <msu4ainiki@gmail.com>
2025-09-29 07:22:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const leftTeamImages = [
 ];
 
 const rightTeamImages = [
-    'https://github.com/msu4ainiki/VS_app/blob/b78a29d593a1bc549fb58a527fe78b9a3383d384/img/Screenshot_20250928-234623~4.png', // PNG с прозрачностью
+    'https://raw.githubusercontent.com/msu4ainiki/VS_app/b78a29d593a1bc549fb58a527fe78b9a3383d384/img/Screenshot_20250928-234623%7E4.png', // PNG с прозрачностью
     'https://disk.yandex.ru/i/tyvL_ZAMsfVJdQ',
     'https://s297klg.storage.yandex.net/rdisk/4c4edb65037a4bc15808a878ee915984f32e664498257496a7cb498d5965bd5e/68da3268/ZQR_a0yN6_t_b6ztngYWwuEL_-X0xkhTt616cGVhMRpAm-HBRUAF2RxqnPJ7FMCwJdRibYJwGY4oOGx40pgARA==?uid=0&filename=Screenshot_20250928-234623~3.png&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&fsize=56804&hid=d851f711f3f4c28424aee760dc52daca&media_type=image&tknv=v3&etag=88ccee64f3f65ae8d9283533760ca7a9&ts=63feb6ba36a00&s=bf71474ec9ffcabab0b88b15903d7e6912b12d87008717d2c833bb87d4d9acb3&pb=U2FsdGVkX1-aAYEQhuGCaq910S-sA34N5N4zegsI0aLto7GLpiIILLIh97vf1zc6ucfeblD_210nL7ehGTWzHHUHnR4YA0tMAf87fMJzo6E'
 ];
