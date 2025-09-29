da20ce56f1cd259b7986dd1269dc0c26058366e6
msu4ainiki <msu4ainiki@gmail.com>
2025-09-29 13:17:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const imageConfig = {
     // Изображения для левой команды (Бегуны)
     leftTeamImages: [
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/Screenshot_20250928-234623~3.png', // PNG с прозрачностью
-    'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/0QLvKh5wGOI~2.jpg',
+    'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/c4cOp7USJBQ~3.jpg',
     'https://s297klg.storage.yandex.net/rdisk/4c4edb65037a4bc15808a878ee915984f32e664498257496a7cb498d5965bd5e/68da3268/ZQR_a0yN6_t_b6ztngYWwuEL_-X0xkhTt616cGVhMRpAm-HBRUAF2RxqnPJ7FMCwJdRibYJwGY4oOGx40pgARA==?uid=0&filename=Screenshot_20250928-234623~3.png&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=0&fsize=56804&hid=d851f711f3f4c28424aee760dc52daca&media_type=image&tknv=v3&etag=88ccee64f3f65ae8d9283533760ca7a9&ts=63feb6ba36a00&s=bf71474ec9ffcabab0b88b15903d7e6912b12d87008717d2c833bb87d4d9acb3&pb=U2FsdGVkX1-aAYEQhuGCaq910S-sA34N5N4zegsI0aLto7GLpiIILLIh97vf1zc6ucfeblD_210nL7ehGTWzHHUHnR4YA0tMAf87fMJzo6E'
 ],
     
