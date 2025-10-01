17f01ef33ae9a1433d3f720e30bc3c0d3f44fd2b
msu4ainiki <msu4ainiki@gmail.com>
2025-10-01 16:47:38 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 const imageConfig = {
     // Изображения для левой команды (Бегуны)
     runners: [
-    'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759205085747_copy_254x296.png', // PNG с прозрачностью
+    // PNG с прозрачностью
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759151720282.png',
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759160744779.png',
         'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759176819638.png'
@@ -16,7 +16,7 @@ const imageConfig = {
     
     // Изображения для правой команды (Лыжники)
     skiers: [
-    'https://raw.githubusercontent.com/msu4ainiki/VS_app/b78a29d593a1bc549fb58a527fe78b9a3383d384/img/Screenshot_20250928-234623%7E4.png', // PNG с прозрачностью
+    // PNG с прозрачностью
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759160143809_copy_504x489.png',
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759143592630.png',
     'https://github.com/msu4ainiki/VS_app/raw/refs/heads/main/img/1759160422940.png',
