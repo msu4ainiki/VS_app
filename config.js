6eac7251a31beaedfae59af7e80e4302b1da0a8c
msu4ainiki <msu4ainiki@gmail.com>
2025-10-02 16:31:12 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,55 +1,28 @@
-// КОНФИГУРАЦИЯ ИЗОБРАЖЕНИЙ - УПРОЩЕННАЯ ВЕРСИЯ
-// БАЗОВЫЙ URL ДЛЯ RAW-ФАЙЛОВ GITHUB
-const baseGitHubUrl = 'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/';
-
-// ДОСТАТОЧНО УКАЗЫВАТЬ ТОЛЬКО ИМЕНА ФАЙЛОВ
+// КОНФИГУРАЦИЯ ИЗОБРАЖЕНИЙ
 const imageConfig = {
-    // Папка с изображениями (если все в одной папке)
-    imagesFolder: '', // Оставьте пустым, если файлы в корне папки img
-    
-    // Изображения для левой команды (Бегуны) - ТОЛЬКО ИМЕНА ФАЙЛОВ
+    // Изображения для левой команды (Бегуны)
     runners: [
-    '1759151720282.png'
-    ,'1759160744779.png'
-    ,'1759387668551_copy_540x671.png'
-    ,'1759176819638.png'
-    ,'1759238335702.png'
-    ,'1759238684512_copy_458x646.png'
-    ,'1759176819638.png'
+     'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759151720282.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759160744779.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759387668551_copy_540x671.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759176819638.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759238335702.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759238684512_copy_458x646.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759176819638.png'
     ],
     
-    // Изображения для правой команды (Лыжники) - ТОЛЬКО ИМЕНА ФАЙЛОВ
+    // Изображения для правой команды (Лыжники)
     skiers: [
-    '1759160143809_copy_504x489.png'
-    ,'1759143592630.png'
-    ,'1759160422940.png'
-    ,'1759176719425.png'
-    ,'1759212657792_copy_343x438.png'
-    ,'1759238483353.png'
-    ,'1759238640181.png'
-    ,'1759238731718_copy_434x681.png'
+     'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759160143809_copy_504x489.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759143592630.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759160422940.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759176719425.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759212657792_copy_343x438.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759238483353.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759238640181.png'
+    ,'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/1759238731718_copy_434x681.png'
     ],
 
-    // Логотип - ТОЛЬКО ИМЯ ФАЙЛА
-    logo: 'bck_sm.png'
+    // Логотип
+    logo: 'https://raw.githubusercontent.com/msu4ainiki/VS_app/main/img/bck_sm.png'
 };
-
-// ФУНКЦИЯ ДЛЯ ФОРМИРОВАНИЯ ПОЛНЫХ URL-АДРЕСОВ
-function getFullImageUrls(filenames, folder = '') {
-    return filenames.map(filename => {
-        const fullUrl = baseGitHubUrl + folder + filename;
-        console.log('Формируем URL:', fullUrl);
-        return fullUrl;
-    });
-}
-
-// ГОТОВЫЕ МАССИВЫ С ПОЛНЫМИ ССЫЛКАМИ
-const runners = getFullImageUrls(imageConfig.runners, imageConfig.imagesFolder);
-const skiers = getFullImageUrls(imageConfig.skiers, imageConfig.imagesFolder);
-const logoUrl = baseGitHubUrl + imageConfig.imagesFolder + imageConfig.logo;
-
-// Экспортируем как глобальные переменные для использования в script.js
-window.imageConfig = imageConfig;
-window.runners = runners;
-window.skiers = skiers;
-window.logoUrl = logoUrl;
